4f225020378ac7080e6ef3b6a3cac99cedc39156
Rachael Gladwin <rachaelgladwin@Rachaels-iMac.local>
2020-04-08 15:55:20 -0600

    working on firebase

--- JS/logic.js
+++ JS/logic.js
@@ -1,55 +1,39 @@
 // Your web app's Firebase configuration
-var config = {
+var firebaseConfig = {
     apiKey: "AIzaSyAghWr1WuGIFliY2NXtRvjd4nJuq9U-Na8",
     authDomain: "portfolioform-cc3ea.firebaseapp.com",
     databaseURL: "https://portfolioform-cc3ea.firebaseio.com",
     projectId: "portfolioform-cc3ea",
     storageBucket: "portfolioform-cc3ea.appspot.com",
     messagingSenderId: "546924526258",
-    appId: "1:546924526258:web:a7f508de569871c7aee7ab",
-    measurementId: "G-TX0GFFLXLQ"
+    appId: "1:546924526258:web:a7f508de569871c7aee7ab"
 };
 // Initialize Firebase
-firebase.initializeApp(config);
+firebase.initializeApp(firebaseConfig);
 var database = firebase.database();
+console.log("here we go");
 
+$("#submit-form").click(function (event) {
+    event.preventDefault();
+    console.log(123);
 
-$("#submit-form").click(function writeUserData(userId, name, phone, email, message) {
-    firebase.database().ref('users/' + userId).set({
+    var name = $("#name").val().trim();
+    var phone = $("#email").val().trim();
+    var email = $("#phone").val().trim();
+    var message = $("#message").val().trim();
+
+    database.ref().set({
         fullNameId: name,
         phoneId: phone,
         emailId: email,
         messageId: message
-    });
+    }), function (errorObject) {
+        console.log("The read failed: " + errorObject.code);
+        alert("Message not sent, see console for error.");
+
+    }
+
+    alert("Message sent!");
+ 
 });
-
-//#region Database functions
-// $("#submit-form").click(function (event) {
-//     event.preventDefault();
-
-
-//     var name = $("#FullNameId").val().trim();
-//     var phone = $("#EmailId").val().trim();
-//     var email = $("#PhoneId").val().trim();
-//     var message = $("#MessageId").val().trim();
-
-//     console.log(EmailId);
-//     console.log(MessageId);
-
-//     database.ref().set({
-//         fullNameId: name,
-//         phoneId: phone,
-//         emailId: email,
-//         messageId: message
-//     }), function (errorObject) {
-//         console.log("The read failed: " + errorObject.code);
-//         alert("Message not sent!")
-//     }
-
-//     alert("Message has been sent!");
-
-// });
-
-// firebase.analytics();
-
 
