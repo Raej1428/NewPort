a79837e8446f6b31da931bf31b6f9e31a1c25175
raej1428 <rachaelgladwin@Rachaels-iMac.local>
2020-06-16 20:15:00 -0600

    moved logic

---
+++ logic.js
@@ -0,0 +1,44 @@
+require("dotenv");
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: process.env.apiKey,
+    authDomain: process.env.authDomain,
+    databaseURL: process.env.databaseURL,
+    projectId: process.env.projectId,
+    storageBucket: process.env.storageBucket,
+    messagingSenderId: process.env.messagingSenderId,
+    appId: process.env.appId
+};
+
+console.log(firebaseConfig);
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+var database = firebase.database();
+console.log("here we go");
+
+$("#submit-form").click(function (event) {
+    event.preventDefault();
+    console.log(123);
+
+    var name = $("#name").val().trim();
+    var phone = $("#email").val().trim();
+    var email = $("#phone").val().trim();
+    var message = $("#message").val().trim();
+   
+
+    database.ref('messages').push({
+        fullName: name,
+        phone: phone,
+        email: email,
+        message: message
+    }), function (errorObject) {
+        console.log("The read failed: " + errorObject.code);
+        alert("Message not sent, see console for error.");
+
+    }
+
+    alert("Message sent!");
+    
+});
+
